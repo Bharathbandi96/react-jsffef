a8b109d26e889c53489951217fc1e5c696f29f2d
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-19 10:49:38 +0530

    3 files modified

--- Examples/Exp.js
+++ Examples/Exp.js
@@ -3,10 +3,24 @@ import Header from './Header'
 import Footer from './Footer'
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: 'welcome',
+    };
+  }
+
+  changeTitle(title){
+    this.setState({title})
+  }
+
 render(){
+  // setTimeout(()=>{
+  //   this.setState({title= 'welcome will'});
+  // },2000)
   return(
     <div>
-      <Header />
+      <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
       <Footer />
     </div>
   );
